8e9610038bee60e508a60b0ed260c7ebd61d0b59
Koffee <engineerkoffee+githb1cbxe1kv@proton.me>
2025-11-08 17:44:37 +1100

    Init Multiplatform project

---
+++ MultiPlatform/composeApp/src/commonMain/kotlin/app/productbrain/Platform.kt
@@ -0,0 +1,7 @@
+package app.productbrain
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
---
+++ MultiPlatform/composeApp/src/iosMain/kotlin/app/productbrain/MainViewController.kt
@@ -0,0 +1,5 @@
+package app.productbrain
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
